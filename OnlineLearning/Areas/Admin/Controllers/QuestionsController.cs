e7f3d4de5cfcfb9f1287a766b7b9a106df7f950e
lxc <572362732@qq.com>
2015-04-19 21:28:10 +0800

    lxc

--- OnlineLearning/Areas/Admin/Controllers/QuestionsController.cs
+++ OnlineLearning/Areas/Admin/Controllers/QuestionsController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineLearning.Areas.Common.Controllers;
 
 namespace OnlineLearning.Areas.Admin.Controllers
 {
-    public class QuestionsController : Controller
+    public class QuestionsController : AdminBaseController
     {
         // GET: Admin/Questions
         public ActionResult Add()
